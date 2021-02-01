b9d6967ff4fc41ade6e5b08109696bfd9bb5de3e
yunahkim <kimy19@wfu.edu>
2021-02-01 00:10:04 -0500

    created py file

---
+++ main.py
@@ -0,0 +1,10 @@
+#These are my messages
+messages_to_roomie = [
+    "i just know you're eating cashews right now",
+    "your daily reminder not to drink two venti cold brew drinks today, not that that would stop you lol",
+    "you are a very cool gal, did you know that",
+    "spencer reid is very proud of you",
+    "please eat something, coffee is not a meal replacement lol",
+    "do you want to go to trader joes <3", 
+    "long-haired harry styles loves you and so do i"
+    ]
